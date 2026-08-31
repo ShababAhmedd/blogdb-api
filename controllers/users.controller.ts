031217b246b59f5a5c4c53883d6bbc3e69635705
ShababAhmedd <shabab.ahmed2000sa@gmail.com>
2026-08-31 21:40:04 +0600

    feat: add updateProfile functionality to allow users to update their profile information

--- controllers/users.controller.ts
+++ controllers/users.controller.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from "express";
 import getErrorMessage from "../utils/getErrorMessage.ts";
 import User from "../models/user.model.ts";
 import type { AuthenticatedRequest } from "../utils/authenticatedRequest.ts";
+import bcrypt from "bcrypt";
 
 export const getUsers = async (req: Request, res: Response) => {
   try {
@@ -104,8 +105,66 @@ export const ownProfile = async (req: AuthenticatedRequest, res: Response) => {
     const findUser = await User.findByPk(userID, {
       attributes: { exclude: ["password"] },
     });
+    if (!findUser) {
+      return res.status(404).json({
+        message: "user not found",
+      });
+    }
+
     res.status(200).json({
       data: findUser,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "server error",
+      data: getErrorMessage(error),
+    });
+  }
+};
+
+export const updateProfile = async (
+  req: AuthenticatedRequest,
+  res: Response,
+) => {
+  try {
+    let { firstName, lastName } = req.body;
+
+    const userID =
+      typeof req.user === "object" && req.user != null
+        ? req.user.id
+        : undefined;
+
+    const findUser = await User.findByPk(userID);
+    if (!findUser) {
+      return res.status(404).json({
+        message: "user not found",
+      });
+    }
+
+    if (!firstName) {
+      firstName = findUser.firstName;
+    }
+
+    if (!lastName) {
+      lastName = findUser.lastName;
+    }
+
+    findUser.firstName = firstName;
+    findUser.lastName = lastName;
+    await findUser.save();
+
+    res.status(200).json({
+      message: "user updated successfully",
+      data: {
+        id: findUser.id,
+        firstName: findUser.firstName,
+        lastName: findUser.lastName,
+        email: findUser.email,
+        isActive: findUser.isActive,
+        role: findUser.role,
+        createdAt: findUser.createdAt,
+        updatedAt: findUser.updatedAt,
+      },
     });
   } catch (error) {
     res.status(500).json({
--- routes/user.route.ts
+++ routes/user.route.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   getUserById,
   getUsers,
+  updateProfile,
   userStatus,
 } from "../controllers/users.controller.ts";
 import { authMiddleWare, isAdmin } from "../middlewares/auth.middleware.ts";
@@ -10,5 +11,6 @@ const router = Router();
 router.get("/api/users", authMiddleWare, isAdmin, getUsers);
 router.get("/api/users/:id", authMiddleWare, isAdmin, getUserById);
 router.patch("/api/users/:id/status", authMiddleWare, isAdmin, userStatus);
+router.put("/api/users/profile/update", authMiddleWare, updateProfile);
 
 export default router;
